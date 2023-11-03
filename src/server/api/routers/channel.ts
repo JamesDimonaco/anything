9bd3846000d8eb0b8966a9369d13434ab695d65d
James <james@whereyouat.co.uk>
2023-11-03 10:00:28 +0000

    create the create route

--- src/server/api/routers/channel.ts
+++ src/server/api/routers/channel.ts
@@ -11,6 +11,29 @@ export const channelRouter = createTRPCRouter({
     return ctx.db.channel.findMany();
   }),
 
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        public: z.boolean(),
+        thumbnailImageUrl: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      console.log("ctx.session.user.id", ctx.session.user.id);
+      console.log(ctx);
+
+      return ctx.db.channel.create({
+        data: {
+          name: input.name,
+          public: input.public,
+          thumbnailImageUrl: input.thumbnailImageUrl,
+          authordBy: { connect: { id: ctx.session.user.id } },
+          members: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+
   // create: protectedProcedure
   //   .input(z.object({ name: z.string().min(1) }))
   //   .mutation(async ({ ctx, input }) => {
