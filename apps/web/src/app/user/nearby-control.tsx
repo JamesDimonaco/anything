3663e30e2f8519d2947bb6642177e60d04b7f1ce
James <james@whereyouat.co.uk>
2023-11-04 21:25:28 +0000

    hmm

---
+++ apps/web/src/app/user/nearby-control.tsx
@@ -0,0 +1,34 @@
+import { api } from "../../trpc/server";
+import UserBlock from "./block-user";
+
+async function NearbyControl() {
+  let members;
+
+  try {
+    const userData = await api.user.getCurrentChannel.query();
+    const data = await api.channel.getMembers.query({
+      channelId: userData?.currentChannelId ?? 420,
+    });
+
+    members = data ? data.members : null;
+  } catch (error) {
+    console.error("Failed to fetch members", error);
+    members = null;
+  }
+
+  if (!members) {
+    return <p>No members found or an error occurred.</p>;
+  }
+
+  return (
+    <div>
+      {members.map((member) => (
+        <div key={member.id}>
+          <UserBlock user={member} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default NearbyControl;
